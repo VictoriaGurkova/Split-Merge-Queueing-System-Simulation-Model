658c01452fece2ae00e49c0e32a399790d7b48bb
ViktoriaGurkova <viktorija.gurkova@mail.ru>
2020-03-08 13:57:06 +0400

    Added TODO

--- Wrapper_for_Devices.py
+++ Wrapper_for_Devices.py
@@ -10,7 +10,7 @@ class Wrapper_for_Devices:
         self.list_of_devices = [Device(self.mu) for _ in range(amount_of_devices)]
 
     # распределяет фрагменты по приборам, устанавливает соответствующее время обслуживания
-    def fragments_distribution(self, demand):
+    def distribute_fragments(self, demand):
         count = 0
         # раскидываем фрагменты по приборам
         for device in self.list_of_devices:
@@ -22,12 +22,12 @@ class Wrapper_for_Devices:
 
     # возвращает количество свободных приборов
     def get_amount_of_free_devices(self):
-        amount = 0
-        for device in self.list_of_devices:
-            # если прибор свободен
-            if device.is_free:
-                amount += 1
-        return amount
+        # amount = 0
+        # for device in self.list_of_devices:
+        #     # если прибор свободен
+        #     if device.is_free:
+        #         amount += 1
+        return (True for device in self.list_of_devices if device.is_free)
 
     # возвращает ближайшее время окончания обслуживания требования
     def get_min_service_duration_for_demand(self):
@@ -40,11 +40,11 @@ class Wrapper_for_Devices:
 
     # возвращает список списков длительностей обслуживания фрагментов каждого требования в сети
     def get_lists_of_service_duration_fragments(self):
-        list_demands_on_devices = self.get_demands_on_devices()
+        list_demands_on_devices = self.get_id_demands_on_devices()
         lists_of_service_duration_fragments = [[] for _ in range(len(list_demands_on_devices))]
         # проходим по списку id требований, которые сейчас находятся на обслуживании
         for id_demand in list_demands_on_devices:
-            # проъходим по всем приборам
+            # проходим по всем приборам
             for device in self.list_of_devices:
                 # если фрагмент на данном приборе принадлежит требованию с id равным id_demand
                 if not device.is_free() and device.fragment.parent_id == id_demand:
@@ -53,11 +53,9 @@ class Wrapper_for_Devices:
                         .append(device.service_duration)
         return lists_of_service_duration_fragments
 
-    def get_demands_on_devices(self):
-        id_demands_on_devices = []
+    def get_id_demands_on_devices(self):
+        id_demands_on_devices = set()
         for device in self.list_of_devices:
-            if not device.is_free() and device.fragment.parent_id not in id_demands_on_devices:
-                id_demands_on_devices.append(device.fragment.parent_id)
-        return id_demands_on_devices
-
-
+            if not device.is_free():
+                id_demands_on_devices.add(device.fragment.parent_id)
+        return list(id_demands_on_devices)
