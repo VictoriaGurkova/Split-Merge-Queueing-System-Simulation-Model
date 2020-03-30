c4ed5073f21359bcb89a45216352be39d198499a
ViktoriaGurkova <viktorija.gurkova@mail.ru>
2020-03-30 10:20:28 +0400

    Refactoring statistic and adding .remove in leaving_demand()

--- Run.py
+++ Run.py
@@ -2,7 +2,7 @@ from SplitMerge import SplitMerge
 from Statistics import Statistics
 
 if __name__ == '__main__':
-    simulation_time = 10000
+    simulation_time = 1000000
     la = 0.5
     mu = 1
     M = 4
