d4ace0028828caea72956af729a34a354ac5263a
Alexey Zavarzin <alex.zavr.27100@mail.ru>
2021-02-16 01:06:09 +0400

    simulation

---
+++ simulation.py
@@ -0,0 +1,21 @@
+from network import SplitMergeSystem
+from network_params import Params
+from progress_bar import ConsoleProgressBar
+
+
+# this code initializes network parameters, starts statistics and performs simulation split-merge network
+if __name__ == '__main__':
+    params = Params(mu=3, lambda1=.5, lambda2=1,
+                    devices_amount=4,
+                    fragments_amounts=[3, 2],
+                    queues_capacities=[10, 30])
+    bar = ConsoleProgressBar('Progress: ')
+
+    model = SplitMergeSystem(params, bar)
+
+    simulation_time = 1000000
+    statistics = model.run(simulation_time)
+
+    # TODO: __str__
+    statistics.show()
+    # statistics.draw_plot()
