338e91add969ade8fc80f8032711cf97922fa8fa
Alexey Zavarzin <alex.zavr.27100@mail.ru>
2021-02-16 01:04:34 +0400

    progress bar / console progress bar

--- progress_bar.py
+++ progress_bar.py
@@ -1,15 +1,24 @@
-# TODO: сделать общий интерфейс для бара
-class ProgressBar:
+from abc import ABCMeta, abstractmethod
 
+
+class ProgressBar:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def update_progress(self, time: float, max_time: float):
+        """Update your progress """
+
+
+class ConsoleProgressBar(ProgressBar):
     __PROGRESS_CHAR = '▓'
     __EMPTY_CHAR = '░'
     __MAX_VALUE = 100
 
-    def __init__(self, current_progress: int, description: str):
+    def __init__(self, description: str, current_progress: int = 0):
         self.current_progress = current_progress
-        print(description, end=" ")
+        print(description)
 
-    def print_progress(self, time: float, max_time: float):
+    def update_progress(self, time: float, max_time: float):
         delta_for_progress = int(100 * time / max_time) - self.current_progress
         if delta_for_progress > 0:
             self.current_progress += delta_for_progress
