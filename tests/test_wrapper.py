fbea14eb1d78ad1507f9fad3403eb1935e01c630
Alexey Zavarzin <alex.zavr.27100@mail.ru>
2021-02-17 19:02:26 +0400

    test for wrapper.py (fail)

---
+++ tests/test_wrapper.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+
+
+class TestDevicesWrapper(TestCase):
+    def test_distribute_fragments(self):
+        self.fail()
+
+    def test_get_amount_of_free_devices(self):
+        self.fail()
+
+    def test_get_min_end_service_time_for_demand(self):
+        self.fail()
+
+    def test_get_service_duration_fragments(self):
+        self.fail()
+
+    def test_get_id_demands_on_devices(self):
+        self.fail()
+
+    def test_get_demand_id_with_min_end_service_time(self):
+        self.fail()
+
+    def test_to_free_demand_fragments(self):
+        self.fail()
+
+    def test_can_occupy(self):
+        self.fail()
+
+    def test_check_if_possible_put_demand_on_devices(self):
+        self.fail()
