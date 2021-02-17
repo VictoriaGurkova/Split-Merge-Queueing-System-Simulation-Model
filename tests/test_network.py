9c07fed428ffc343054bc14863970e68ead22909
Alexey Zavarzin <alex.zavr.27100@mail.ru>
2021-02-17 19:02:03 +0400

    test for network.py (fail)

---
+++ tests/test_network.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+
+class TestSplitMergeSystem(TestCase):
+    def test_run(self):
+        self.fail()
+
+    def test__arrival_of_demand(self):
+        self.fail()
+
+    def test__demand_service_start(self):
+        self.fail()
+
+    def test__leaving_demand(self):
+        self.fail()
+
+    def test__set_events_times(self):
+        self.fail()
+
+    def test__define_arriving_demand_class(self):
+        self.fail()
