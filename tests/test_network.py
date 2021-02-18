0a511a6949068f7015ac19c5ac62c944f9b8bcd3
Alexey Zavarzin <alex.zavr.27100@mail.ru>
2021-02-18 23:33:46 +0400

    test for network

--- tests/test_network.py
+++ tests/test_network.py
@@ -1,22 +1,31 @@
 from unittest import TestCase
+from unittest import skip
+
+from network import SplitMergeSystem
 
 
 class TestSplitMergeSystem(TestCase):
 
+    @skip
     def test_run(self):
         self.fail()
 
+    @skip
     def test__arrival_of_demand(self):
         self.fail()
 
+    @skip
     def test__demand_service_start(self):
         self.fail()
 
+    @skip
     def test__leaving_demand(self):
         self.fail()
 
+    @skip
     def test__set_events_times(self):
         self.fail()
 
     def test__define_arriving_demand_class(self):
-        self.fail()
+        self.assertEqual(SplitMergeSystem._define_arriving_demand_class(probability=1), 0)
+        self.assertEqual(SplitMergeSystem._define_arriving_demand_class(probability=0), 1)
