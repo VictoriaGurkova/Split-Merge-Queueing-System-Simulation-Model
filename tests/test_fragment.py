2df6fd0f4613a72502257649f7193acee7cc0d81
Alexey Zavarzin <alex.zavr.27100@mail.ru>
2021-02-19 00:11:20 +0400

    tests pass

--- tests/test_fragment.py
+++ tests/test_fragment.py
@@ -6,6 +6,7 @@ from entities.fragment import Fragment
 class TestFragment(TestCase):
 
     def test_checking_correct_operation_of_counter_id(self):
+        Fragment._reset_counter()
         fragment0 = Fragment(parent_id=0)
         self.assertEqual(fragment0.id, 0)
 
